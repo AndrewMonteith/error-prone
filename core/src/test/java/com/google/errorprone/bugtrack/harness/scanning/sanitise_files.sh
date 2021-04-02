059949b3b45345523b9d59c564e1a6aaa865352f
AndrewMonteith <andrewmonteith1999@gmail.com>
2021-04-02 22:03:12 +0100

    Heuristics for making files format

--- core/src/test/java/com/google/errorprone/bugtrack/harness/scanning/sanitise_files.sh
+++ core/src/test/java/com/google/errorprone/bugtrack/harness/scanning/sanitise_files.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
+for file in "$@"
+do
+  sed -i "s/_)/__)/g" "$file"
+  sed -i "s/_,/__,/g" "$file"
+  sed -i "s/_ ->/ __ ->/g" "$file"
+done
+
 java -jar "../google-format.jar" -i --skip-sorting-imports --skip-removing-unused-imports "$@"
